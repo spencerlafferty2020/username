5a33aea8fef3f21499a01e45b498777f10f43fea
Spencer Lafferty <lafferty@chapman.edu>
2020-11-11 10:42:43 -0800

    Added the rules for a valid username to username.sh

--- username.sh
+++ username.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # username.sh
 # Spencer Lafferty
-echo "Enter a username: "
+echo "Enter a username (Only letters, numbers, and underscores): "
 read UM
-while echo "$UM" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo "$UM" | egrep -v "^[a-z][0-9][_]{3,12}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid username - Only 3 to 12 letters, numbers, and underscores!"
+	echo "Enter a valid username: "
 	read UM
 done
 echo "Thank you"
